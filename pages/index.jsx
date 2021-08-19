e9644a5f6f73e1bf4a22d349c5613fab92496be3
abhishek-dangol <abhi.dangol@gmail.com>
2021-08-18 22:53:00 -0500

    created static props for gifs from giphy API

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,50 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 
-export default function Home() {
+export default function Home(initialData) {
+  // useEffect(() => {
+  //   console.log(initialData);
+  // })
   return (
     <div className='container'>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/styles.css"/>
+        <link rel="stylesheet" href="/styles.css" />
       </Head>
+      
       <h1>Giphy Search App</h1>
+
+      <form>
+        <input onChange={() => console.log('happens')} type="text"/>
+      </form>
+
+      <div class="giphy-search-results-grid">
+        {initialData.catGiphys.data.map((each, index) => {
+          return (
+            <div key="index">
+              <h3>{each.title}</h3>
+              <img src={each.images.original.url} alt={each.title} />
+            </div>
+          )
+        })}
+      </div>
     </div>
+  
   )
+}
+
+
+
+export async function getStaticProps() {
+  let catGiphys = await fetch(
+    "https://api.giphy.com/v1/gifs/search?q=funny&api_key=z7VPExeiLKstC33626F2lZMD2nceySKs&limit=10"
+  );
+  catGiphys = await catGiphys.json();
+  return {
+    props: {
+      catGiphys: catGiphys
+    }
+  }
 }
